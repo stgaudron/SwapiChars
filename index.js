4059960ba9af7115c730d4f9e3c30ceda6f2c05f
stgaudron <stgaudron@gmail.com>
2020-06-24 20:19:23 +0200

    indexjs

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const db = require('./models/db');
 const cors = require('cors');
 const express = require('express');
 const charController = require('./controllers/charController');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const path = require('path');
 
-var router = express.Router();
 var app = express();
 require('events').EventEmitter.prototype._maxListeners = 100;
 
@@ -14,6 +14,11 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.use('/char', charController);
 
+// serve static website
+app.use(express.static(path.join(__dirname, "./frontend/build")));
+
+// redirect unknown pages to homepage
+app.get("/*", (req, res) => res.sendFile(path.join(__dirname, "./frontend/build", "index.html")));
 
 app.listen(4000, () => {
     console.log('Express server started at port : 4000');
