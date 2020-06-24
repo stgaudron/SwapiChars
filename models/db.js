b6aaec7c46d5e52f1669a91a51c8507d653689d5
stgaudron <stgaudron@gmail.com>
2020-06-24 18:59:16 +0200

    dbconparam

--- models/db.js
+++ models/db.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://stgaudron:swapitest@cluster0-zom7f.mongodb.net/Swapichars?retryWrites=true&w=majority', { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }, (err) => {
+const connectionString = process.env.MONGO_CONNECTION_STRING || 'mongodb+srv://stgaudron:swapitest@cluster0-zom7f.mongodb.net/Swapichars?retryWrites=true&w=majority';
+
+mongoose.connect(connectionString, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }, (err) => {
     if (!err) { console.log('MongoDB Connection Succeeded.') }
     else { console.log('Error in DB connection : ' + err) }
 });
